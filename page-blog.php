6c7b9452d3b6ca120130e2ac34f57ae815473d6b
Nicolas Widart <n.widart@gmail.com>
2012-02-10 21:00:32 +0100

    Blog
    # More blog additions
    
    Signed-off-by: Nicolas Widart <n.widart@gmail.com>

--- page-blog.php
+++ page-blog.php
@@ -15,77 +15,58 @@ Template Name: Blog
 ?>
 
   <div class="row">
-  	<div class="ninecol entrycontent">
-  	<?php // ??post_class(); ?>
-  		<h2 class="post"><a href="<?php echo get_permalink() ?>" title="Permanent Link: <?php the_title(); ?>"><?php the_title(); ?></a></h2
+  	<div class="ninecol entrycontent"  id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+<?php
+      // getting the post type 
+      $blog_post_type = get_post_meta($post->ID, 'blog_post_type', true);
+?>  	
+  		<h2 class="<?php echo $blog_post_type; ?>"><a href="<?php echo get_permalink() ?>" title="Permanent Link: <?php the_title(); ?>"><?php the_title(); ?></a></h2>
 
   		<?php
+      //getting thumbnail meta
   		$thumbnail = get_post_meta($post->ID, 'blog_image', true);
-  		echo $thumbnail;
+  		
 
-  		if (get_post_meta($post->ID, 'thumbnail', true)) {  ?>
-  			<img src="<?php echo get_post_meta($post->ID, 'thumbnail', true); ?>" alt="img1" class="blog-post-cover-img"/>
+  		if ($thumbnail) {  ?>
+  			<img src="<?php echo bloginfo('template_directory'); echo $thumbnail; ?>" alt="<?php the_title(); ?>" class="blog-post-cover-img"/>
   		<?php } ?>
   		<div class="eightcol">
-  			<!-- <p>
-  				First, I believe that this nation should commit itself to achieving the goal, before this decade is out, of landing a man on the moon and returning him safely to the earth. No single space project in this period will be more impressive to mankind, or more important for the long-range exploration of space; and none will be so difficult or expensive to accomplish.
-  			</p> -->
   			<?php the_excerpt(''); ?>
   			<p>
-  				<a href="">Continue reading →</a>
+  				<a href="<?php echo get_permalink() ?>">Continue reading →</a>
   			</p>
   			
   		</div>
   		<div class="threecol">
   			<ul>
   				<li>
-  					<p><span>Tags:</span> <a href="">Design</a>, <a href="">Html</a>, <a href="">Wordpress</a></p>
+            <p><i class="icon-tags"></i> <?php the_tags(' ', ', ', ' '); ?></p>
+          </li>
+  				<li>
+  					<p><i class="icon-user"></i> <!-- <span>Author:</span> --> <a href=""><?php echo get_the_author(); ?></a></p>
   				</li>
   				<li>
-  					<p><span>Author:</span> <a href="">Nicolas</a></p>
+  					<p><i class="icon-time"></i> <!-- <span>Date:</span> --> <?php the_time('d F, Y'); ?></p>
   				</li>
   				<li>
-  					<p><span>Date:</span> <a href="">23 september, 2012</a></p>
+  					<p><i class="icon-list-alt"></i> <!-- <span>Category:</span> --> <a href=""><?php the_category(', ') ?></a></p>
   				</li>
   				<li>
-  					<p><span>Category:</span> <a href="">Articles</a></p>
-  				</li>
-  				<li>
-  					<p><span>Comments:</span> <a href="">2</a></p>
+  					<p><i class="icon-comment"></i> <!-- <span>Comments:</span> --> <a href=""><?php comments_popup_link('No Comments', '1 Comment', '% Comments'); ?></a></p>
   				</li>
   			</ul>
   		</div>
   	
-  	<div class="seperator"></div>	
+
+    <?php
+    if (($wp_query->current_post + 1) < ($wp_query->post_count)) {
+       echo '<div class="seperator"></div>';
+    }
+    ?>
+  	
   	</div>
 
-    <div class="threecol last sidebar">
-      <ul>
-				<form role="search" method="get" id="searchform" action="http://nicolaswidart.com/site/">
-					<div>
-						<input type="text" value="" name="s" id="s" class="search-input" placeholder="Type something you want to find"/>
-						<!-- <input type="submit" id="searchsubmit" value="Go" class="search-go"/> -->
-					</div>
-				</form>
-			</ul>
-			<h3>Welcome on my blog!</h3>
-			<p>First, I believe that this nation should commit itself to achieving the goal, before this decade is out, of landing a man on the moon and returning him safely to the earth.</p>
-			<h3>Categories</h3>
-			<ul class="ul-list">
-				<li><a href="">Technology</a></li>
-				<li><a href="">Tips & tricks</a>
-					<ul>
-						<li><a href="">Html & Css</a></li>
-						<li><a href="">Wordpress</a></li>
-						<li><a href="">jQuery</a></li>
-						<li><a href="">PHP</a></li>
-					</ul>
-				</li>
-				<li><a href="">Uncategorized</a></li>
-			</ul>
-			
-			
-    </div><!-- end sidebar -->
+    <?php get_sidebar(); ?>
     <?php endwhile; ?>
 	<?php wp_reset_postdata(); // reset the query ?>
   </div><!-- end row -->  
---
+++ sidebar.php
@@ -0,0 +1,27 @@
+<div class="threecol last sidebar">
+      <ul>
+				<form role="search" method="get" id="searchform" action="http://nicolaswidart.com/site/">
+					<div>
+						<input type="text" value="" name="s" id="s" class="search-input" placeholder="Type something you want to find"/>
+						<!-- <input type="submit" id="searchsubmit" value="Go" class="search-go"/> -->
+					</div>
+				</form>
+			</ul>
+			<h3>Welcome on my blog!</h3>
+			<p>First, I believe that this nation should commit itself to achieving the goal, before this decade is out, of landing a man on the moon and returning him safely to the earth.</p>
+			<h3>Categories</h3>
+			<ul class="ul-list">
+				<li><a href="">Technology</a></li>
+				<li><a href="">Tips & tricks</a>
+					<ul>
+						<li><a href="">Html & Css</a></li>
+						<li><a href="">Wordpress</a></li>
+						<li><a href="">jQuery</a></li>
+						<li><a href="">PHP</a></li>
+					</ul>
+				</li>
+				<li><a href="">Uncategorized</a></li>
+			</ul>
+			
+			
+    </div><!-- end sidebar -->
